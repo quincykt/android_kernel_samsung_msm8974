da37db89f14864f3e7d52709f7ece8a3ab17c951
Park Ju Hyung <qkrwngud825@gmail.com>
2015-09-22 00:28:09 +0900

    Revert "ramdisk: enable lazyplug"
    This reverts commit 464a82774804d4dc0cf04bae809cc25bda1bf97c.
    
    While I still think this concept makes sense, cpufreq should be updated
    with the new behavior.
    Seems like cpufreq scales lazily when all cores are on, so responsiveness
    is reduced.
    
    However, workload that favors higher number of cores can still benefit from
    lazyplug.
    So leave these options for users to choose.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/sbin/boot.sh
+++ ramdisk/sbin/boot.sh
@@ -2,7 +2,7 @@
 
 export PATH=/res/asset:$PATH
 
-echo "1" > /sys/module/lazyplug/parameters/lazyplug_active
+echo "1" > /sys/module/intelli_plug/parameters/intelli_plug_active
 echo "1" > /sys/module/msm_thermal/parameters/enabled
 echo "0" > /sys/module/msm_thermal/core_control/enabled
 echo "1" > /sys/devices/system/cpu/cpu1/online
