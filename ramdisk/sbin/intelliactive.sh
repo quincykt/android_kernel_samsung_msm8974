d53133eb7d0aad3fc0fb90ffffc49dc3fc98bf04
Park Ju Hyung <qkrwngud825@gmail.com>
2015-11-03 20:54:31 +0900

    ramdisk: use lower go_hispeed_load and enable io_is_busy
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/sbin/intelliactive.sh
+++ ramdisk/sbin/intelliactive.sh
@@ -11,7 +11,8 @@ echo "20000"	> /sys/devices/system/cpu/cpufreq/intelliactive/timer_rate
 echo "20000"	> /sys/devices/system/cpu/cpufreq/intelliactive/timer_slack
 echo "40000"	> /sys/devices/system/cpu/cpufreq/intelliactive/min_sample_time
 echo "1190400"	> /sys/devices/system/cpu/cpufreq/intelliactive/hispeed_freq
-echo "99"	> /sys/devices/system/cpu/cpufreq/intelliactive/go_hispeed_load
+echo "90"	> /sys/devices/system/cpu/cpufreq/intelliactive/go_hispeed_load
 echo "20000 1400000:80000 1500000:40000 1700000:20000"	> /sys/devices/system/cpu/cpufreq/intelliactive/above_hispeed_delay
 echo "85 1400000:90 1700000:95"	> /sys/devices/system/cpu/cpufreq/intelliactive/target_loads
 echo "100000"	> /sys/devices/system/cpu/cpufreq/intelliactive/sampling_down_factor
+echo "1"	> /sys/devices/system/cpu/cpufreq/intelliactive/io_is_busy
